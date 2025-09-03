092eb0485d78be957952883bd7cc72bd4dcdbf7e
ariiffff <muhmdarif3@gmail.com>
2025-09-04 06:51:33 +0700

    make component home views

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Sidebar from './components/Sidebar.vue';
+import Hero from './components/Hero.vue';
+import HomeCard from './components/HomeCard.vue';
+import JobListings from './components/JobListings.vue';
+
+
+</script>
+
+<template>
+  <Sidebar />
+  <Hero title="Menjadi Developer Vue" subtitle="Temukan Pekerjaan yang cocok dengan skillmu" />
+  <HomeCard />
+  <JobListings limit="3" showButton="true"/>
+
+
+
+</template>
+
---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+
+<script setup>
+import {defineProps} from 'vue';
+
+defineProps({
+    bg: {
+        type : String,
+        default : "bg-gray-100"
+    }
+})
+</script>
+
+
+
+<template>
+    <div class="p-6 rounded-lg shadow-md" :class="bg">
+        <slot></slot>
+    </div>
+
+</template>
